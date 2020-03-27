22c1a2e3a1d73919dc10f0a5ed8b9b74723cbda7
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-27 11:09:30 +0100

    ajout des commandes sql et creation de deux tables

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ const settings = require("./settings.json");
 const sqlConfig = settings.sqlConfig;
 
 const app = express();
+app.use(express.static("./public"));
 
 app.listen(3000, () => {
     console.log("LE SERVEUR DÉMARRE ...  !");
 });
+app.get("/", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+
+    sqlConnection.query(
+        "INSERT INTO node_users VALUES (NULL, ?, ?, ?, ?, ?)",
+        [req.body.email, req.body.password, req.body.firstname, req.body.lastname, req.body.birthdate],
+        (error, result) => {
+            if (error) {
+                console.log("ERROR :", error.code);
+                res.status(503).send({ status: "ERROR" });
+            } else {
+                console.log(result);
+                res.send({ status: "OK" });
+            }
+            sqlConnection.end();
+        });
+});
+
+
+app.use(express.urlencoded({ extended: true }));
