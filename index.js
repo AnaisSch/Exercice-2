6a118672ac74e2f6450c9641457664a6833e3b88
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-27 10:10:50 +0100

    ajout fichier index.js et settings.json pour mysql

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require(express);
+const mysql = require("mysql");
+
+const settings = require("./settings.json");
+const sqlConfig = settings.sqlConfig;
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("LE SERVEUR DÉMARRE ...  !");
+});
